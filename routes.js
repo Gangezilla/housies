747dada0e6b78379d59318ae52230b3d4894bc89
Scott <scottgangemi@gmail.com>
2017-10-24 20:14:23 +1100

    '

--- routes.js
+++ routes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
   // res.send('home');
 });
 
-router.get('/auth/facebook', passport.authenticate('facebook'));
+router.get('/auth/facebook/', passport.authenticate('facebook'));
 
 router.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
